cedcae55529d8a2d7636e481d826d5381951f855
mdmasumbillah00 <msmasumbillah0000@gmail.com>
2022-03-31 20:49:51 +0800

    first commit

---
+++ shoping-mall/script.js
@@ -0,0 +1,19 @@
+var catagoris = document.getElementsByClassName("catagori");
+catagoris[0].addEventListener("click", function () {
+    alert("mobile")
+});
+catagoris[1].addEventListener("click", function () {
+    alert("laptop")
+});
+catagoris[2].addEventListener("click", function () {
+    alert("shirt")
+});
+catagoris[3].addEventListener("click", function () {
+    alert("dress")
+});
+catagoris[4].addEventListener("click", function () {
+    alert("shoe")
+});
+catagoris[5].addEventListener("click", function () {
+    alert("home-application")
+});
