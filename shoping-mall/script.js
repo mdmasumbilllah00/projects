12e062f5814d5bfd9d467abbc4f6b3feb0b946a9
mdmasumbillah00 <msmasumbillah0000@gmail.com>
2022-04-01 20:49:35 +0800

    done main thing

--- shoping-mall/script.js
+++ shoping-mall/script.js
@@ -1,24 +1,91 @@
 var catagoris = document.getElementsByClassName("catagori");
 catagoris[0].addEventListener("click", function () {
-    var x = document.getElementById("mobile").innerHTML;
-    document.getElementById("display").innerHTML = x;
+    document.getElementById("display").innerHTML = ""
+    var x = document.getElementById("mobile");
+    var Products = x.getElementsByClassName("products");
+    var dis = document.getElementById("display");
+    for (var product of Products) {
+        dis.innerHTML += product.innerHTML;
+    }
+    var atr = document.getElementById("display").attributes.getNamedItem("class");
+    atr.value = "row row-cols-1 row-cols-lg-4 row-cols-md-2    g-4";
     document.getElementById("second-display").style.display = "none";
 
 });
 catagoris[1].addEventListener("click", function () {
-    var x = document.getElementById("laptop").innerHTML;
-    document.getElementById("display").innerHTML = x;
+    document.getElementById("display").innerHTML = ""
+    var x = document.getElementById("laptop");
+    var Products = x.getElementsByClassName("products");
+    var dis = document.getElementById("display");
+    for (var product of Products) {
+        dis.innerHTML += product.innerHTML;
+    }
+    var atr = document.getElementById("display").attributes.getNamedItem("class");
+    atr.value = "row row-cols-1 row-cols-lg-3 row-cols-md-2   g-4";
     document.getElementById("second-display").style.display = "none";
+
 });
 catagoris[2].addEventListener("click", function () {
-    alert("shirt")
+    document.getElementById("display").innerHTML = ""
+    var x = document.getElementById("man-shirt");
+    var Products = x.getElementsByClassName("products");
+    var dis = document.getElementById("display");
+    for (var product of Products) {
+        product.style.maxWidth = "80%";
+        product.style.margin = "5px";
+
+        dis.innerHTML += product.innerHTML;
+    }
+    var atr = document.getElementById("display").attributes.getNamedItem("class");
+    atr.value = "row row-cols-1 row-cols-lg-4 row-cols-md-2 m-5     g-4";
+    document.getElementById("second-display").style.display = "none";
+
 });
 catagoris[3].addEventListener("click", function () {
-    alert("dress")
+    document.getElementById("display").innerHTML = ""
+    var x = document.getElementById("women-clothing");
+    var Products = x.getElementsByClassName("products");
+    var dis = document.getElementById("display");
+    for (var product of Products) {
+        product.style.maxWidth = "80%";
+        product.style.margin = "5px";
+
+        dis.innerHTML += product.innerHTML;
+    }
+    var atr = document.getElementById("display").attributes.getNamedItem("class");
+    atr.value = "row row-cols-1 row-cols-lg-4 row-cols-md-2 m-5     g-4";
+    document.getElementById("second-display").style.display = "none";
+
 });
 catagoris[4].addEventListener("click", function () {
-    alert("shoe")
+    document.getElementById("display").innerHTML = ""
+    var x = document.getElementById("man-shoes");
+    var Products = x.getElementsByClassName("products");
+    var dis = document.getElementById("display");
+    for (var product of Products) {
+        product.style.maxWidth = "80%";
+        product.style.margin = "5px";
+
+        dis.innerHTML += product.innerHTML;
+    }
+    var atr = document.getElementById("display").attributes.getNamedItem("class");
+    atr.value = "row row-cols-1 row-cols-lg-4 row-cols-md-2 m-5     g-4";
+    document.getElementById("second-display").style.display = "none";
+
 });
 catagoris[5].addEventListener("click", function () {
-    alert("home-application")
+    document.getElementById("display").innerHTML = ""
+    var x = document.getElementById("home-application");
+    var Products = x.getElementsByClassName("products");
+    var dis = document.getElementById("display");
+    for (var product of Products) {
+        product.style.maxWidth = "80%";
+        product.style.margin = "5px";
+
+        dis.innerHTML += product.innerHTML;
+    }
+    var atr = document.getElementById("display").attributes.getNamedItem("class");
+    atr.value = "row row-cols-1 row-cols-lg-4 row-cols-md-2 m-5     g-4";
+    document.getElementById("second-display").style.display = "none";
+
 });
