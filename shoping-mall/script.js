934a74f9c8f5359133a9118ac3d9d6c2f65c5797
mdmasumbillah00 <msmasumbillah0000@gmail.com>
2022-04-01 21:53:48 +0800

    first section done

--- shoping-mall/script.js
+++ shoping-mall/script.js
@@ -89,3 +89,16 @@ catagoris[5].addEventListener("click", function () {
     document.getElementById("second-display").style.display = "none";
 
 });
+catagoris[6].addEventListener("click", function () {
+    document.getElementById("display").innerHTML = ""
+    var Products = document.getElementsByClassName("products");
+    var dis = document.getElementById("display");
+    var number = Math.round(Math.random() * 20)
+    for (var i = number; i < Products.length; i += 8) {
+        dis.innerHTML += Products[i].innerHTML;
+    }
+    var atr = document.getElementById("display").attributes.getNamedItem("class");
+    atr.value = "row row-cols-1 row-cols-lg-3 row-cols-md-2 m-5     g-4";
+    document.getElementById("second-display").style.display = "none";
+
+});
